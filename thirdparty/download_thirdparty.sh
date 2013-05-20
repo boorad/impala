b06748d8d766f0540484e714ca4c4018819bf8e9
Brad Anderson <brad@sankatygroup.com>
2013-05-20 11:43:12 -0400

    comment out some download_thirdparty stuffs

--- thirdparty/download_thirdparty.sh
+++ thirdparty/download_thirdparty.sh
@@ -20,8 +20,9 @@ fi
 
 cd ${IMPALA_HOME}/thirdparty
 
-echo "Removing everything in ${IMPALA_HOME}/thirdparty"
-git clean -xdf
+# TODO: mapr tarballs, then re-enable this
+#echo "Removing everything in ${IMPALA_HOME}/thirdparty"
+#git clean -xdf
 
 echo "Fetching gtest"
 wget http://googletest.googlecode.com/files/gtest-${IMPALA_GTEST_VERSION}.zip
@@ -58,10 +59,11 @@ wget http://mongoose.googlecode.com/files/mongoose-${IMPALA_MONGOOSE_VERSION}.tg
 tar xzf mongoose-${IMPALA_MONGOOSE_VERSION}.tgz
 rm mongoose-${IMPALA_MONGOOSE_VERSION}.tgz
 
-echo "Fetching Apache Hive"
-wget http://archive.cloudera.com/cdh4/cdh/4/hive-${IMPALA_HIVE_VERSION}.tar.gz
-tar xzf hive-${IMPALA_HIVE_VERSION}.tar.gz
-rm hive-${IMPALA_HIVE_VERSION}.tar.gz
+# TODO: mapr tarball
+#echo "Fetching Apache Hive"
+#wget http://archive.cloudera.com/cdh4/cdh/4/hive-${IMPALA_HIVE_VERSION}.tar.gz
+#tar xzf hive-${IMPALA_HIVE_VERSION}.tar.gz
+#rm hive-${IMPALA_HIVE_VERSION}.tar.gz
 
 echo "Fetching Apache Thrift"
 wget http://archive.apache.org/dist/thrift/${IMPALA_THRIFT_VERSION}/thrift-${IMPALA_THRIFT_VERSION}.tar.gz
