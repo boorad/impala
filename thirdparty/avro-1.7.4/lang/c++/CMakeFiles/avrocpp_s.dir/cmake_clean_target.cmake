c863e52d78f3079d225c5937d0dad98462231648
Brad Anderson <brad@sankatygroup.com>
2013-06-01 11:00:11 -0400

    zap hive/avro from repo

--- thirdparty/avro-1.7.4/lang/c++/CMakeFiles/avrocpp_s.dir/cmake_clean_target.cmake
+++
@@ -1,5 +0,0 @@
-FILE(REMOVE_RECURSE
-  "libavrocpp_s.a"
-  "libavrocpp_s.so.1.7.4.0"
-  "libavrocpp_s.so"
-)
--- thirdparty/hive-0.10.0-mapr/scripts/metastore/upgrade/mysql/011-HIVE-3649.mysql.sql
+++
@@ -1,2 +0,0 @@
-SELECT '< HIVE-3649: Support stored as directories >' AS ' ';
-ALTER TABLE `SDS` ADD `IS_STOREDASSUBDIRECTORIES` bit(1) ;
